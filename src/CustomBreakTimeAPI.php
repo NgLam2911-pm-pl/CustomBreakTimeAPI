0b5efd1dc57cbc5ed3fe879e4e60fdaf23649600
NgLamIsSimp <nglamvn2911@gmail.com>
2021-09-20 17:43:33 +0700

    API4 changes (p2)

--- src/CustomBreakTimeAPI.php
+++ src/CustomBreakTimeAPI.php
@@ -1,53 +1,46 @@
 <?php
+declare(strict_types=1);
 
-namespace malgn\CustomBreakTimeAPI;
+namespace NgLamVN\CustomBreakTimeAPI;
 
-use malgn\test\Test;
 use pocketmine\item\Item;
-use pocketmine\nbt\BigEndianNBTStream;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use RuntimeException;
 
-class CustomBreakTimeAPI extends PluginBase
-{
-    /** @var BaseBreakTime[] $data */
-    public static $data = [];
-    /** @var bool[] */
-    protected $breaking = [];
+class CustomBreakTimeAPI extends PluginBase{
+	/** @var BaseBreakTime[] $data */
+	public static array $data = [];
+	/** @var bool[] */
+	protected array $breaking = [];
 
-    public function isBreaking(Player $player): bool
-    {
-        if (!isset($this->breaking[$player->getName()])) return false;
-        else return $this->breaking[$player->getName()];
-    }
+	public function isBreaking(Player $player) : bool{
+		if(!isset($this->breaking[$player->getName()])) return false;
+		else return $this->breaking[$player->getName()];
+	}
 
-    public function setBreakStatus(Player $player, bool $status)
-    {
-        $this->breaking[$player->getName()] = $status;
-    }
+	public function setBreakStatus(Player $player, bool $status){
+		$this->breaking[$player->getName()] = $status;
+	}
 
-    public static function register(BaseBreakTime $baseBreakTime)
-    {
-        $name = $baseBreakTime->getName();
-        if (isset(self::$data[$name])) throw new \Exception("A BaseBreakTime config with that name aldready registered !");
-        self::$data[$name] = $baseBreakTime;
-    }
+	/**
+	 * @throws RuntimeException
+	 */
+	public static function register(BaseBreakTime $baseBreakTime){
+		$name = $baseBreakTime->getName();
+		if(isset(self::$data[$name])) throw new RuntimeException("A BaseBreakTime config with that name aldready registered !");
+		self::$data[$name] = $baseBreakTime;
+	}
 
-    public static function getBaseBreakTime(Item $item): ?BaseBreakTime
-    {
-        $nbt = $item->getNamedTag();
-        if (!$nbt->hasTag("basebreaktime")) return null;
-        $config = $nbt->getString("basebreaktime");
+	public static function getBaseBreakTime(Item $item) : ?BaseBreakTime{
+		$nbt = $item->getNamedTag();
+		if(!$nbt->getTag("basebreaktime") == null) return null;
+		$config = $nbt->getString("basebreaktime");
 
-        if (isset(self::$data[$config]))
-        {
-            return self::$data[$config];
-        }
-        else return null;
-    }
+		return self::$data[$config] ?? null;
+	}
 
-    public function onEnable()
-    {
-        $this->getServer()->getPluginManager()->registerEvents(new EventHandler($this), $this);
-    }
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents(new EventHandler($this), $this);
+	}
 }
