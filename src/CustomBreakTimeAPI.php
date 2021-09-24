d8d42f64dcf0e15d646266632b8d78c1492d49ec
NgLamIsSimp <nglamvn2911@gmail.com>
2021-09-24 11:17:12 +0700

    Use SimplePacketHandler to handle packet.

--- src/CustomBreakTimeAPI.php
+++ src/CustomBreakTimeAPI.php
@@ -3,7 +3,14 @@ declare(strict_types=1);
 
 namespace NgLamVN\CustomBreakTimeAPI;
 
+use Closure;
+use muqsit\simplepackethandler\monitor\IPacketMonitor;
+use muqsit\simplepackethandler\SimplePacketHandler;
 use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\NetworkSession;
+use pocketmine\network\mcpe\protocol\PlayerActionPacket;
+use pocketmine\player\GameMode;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use RuntimeException;
@@ -13,6 +20,10 @@ class CustomBreakTimeAPI extends PluginBase{
 	public static array $data = [];
 	/** @var bool[] */
 	protected array $breaking = [];
+	/** @var BreakTask[] */
+	protected array $task = [];
+
+	protected IPacketMonitor $packet_monitor;
 
 	public function isBreaking(Player $player) : bool{
 		if(!isset($this->breaking[$player->getName()])) return false;
@@ -42,5 +53,34 @@ class CustomBreakTimeAPI extends PluginBase{
 
 	public function onEnable() : void{
 		$this->getServer()->getPluginManager()->registerEvents(new EventHandler($this), $this);
+		$this->packet_monitor = SimplePacketHandler::createMonitor($this);
+		$this->packet_monitor->monitorIncoming(Closure::fromCallable([$this, "handleMonitorPacket"]));
+	}
+
+	public function handleMonitorPacket(PlayerActionPacket $packet, NetworkSession $session) : void{
+		$player = $session->getPlayer();
+		if($player->getGamemode() !== GameMode::SURVIVAL()){
+			return;
+		}
+		switch($packet->action){
+			case PlayerActionPacket::ACTION_START_BREAK:
+				$item = $player->getInventory()->getItemInHand();
+				$basetime = CustomBreakTimeAPI::getBaseBreakTime($item);
+				if($basetime == null) return;
+				$pos = new Vector3($packet->x, $packet->y, $packet->z);
+				$block = $player->getWorld()->getBlock($pos);
+				$time = $basetime->reCaculateBreakTime($block, $item, $player);
+				$this->setBreakStatus($player, true);
+				$this->task[$player->getName()] = new BreakTask($player, $pos, $this);
+				$this->getScheduler()->scheduleDelayedTask($this->task[$player->getName()], $time);
+				break;
+			case PlayerActionPacket::ACTION_ABORT_BREAK:
+			case PlayerActionPacket::ACTION_STOP_BREAK:
+				$this->setBreakStatus($player, false);
+				if(isset($this->task[$player->getName()])){
+					$this->task[$player->getName()]->cancel();
+				}
+				break;
+		}
 	}
 }
--- src/EventHandler.php
+++ src/EventHandler.php
@@ -13,8 +13,6 @@ use pocketmine\player\GameMode;
 class EventHandler implements Listener{
 	/** @var CustomBreakTimeAPI $api */
 	protected CustomBreakTimeAPI $api;
-	/** @var BreakTask[] */
-	protected array $task = [];
 
 	public function __construct(CustomBreakTimeAPI $api){
 		$this->api = $api;
@@ -22,40 +20,6 @@ class EventHandler implements Listener{
 
 	public function getAPI() : CustomBreakTimeAPI{
 		return $this->api;
-	}
-
-	/**
-	 * @param DataPacketReceiveEvent $event
-	 * @priority LOWEST
-	 * @handleCancelled FALSE
-	 */
-	public function onRecieve(DataPacketReceiveEvent $event) : void{
-		$packet = $event->getPacket();
-		if(!$packet instanceof PlayerActionPacket) return;
-		$player = $event->getOrigin()->getPlayer();
-		if($player->getGamemode() !== GameMode::SURVIVAL()){
-			return;
-		}
-		switch($packet->action){
-			case PlayerActionPacket::ACTION_START_BREAK:
-				$item = $player->getInventory()->getItemInHand();
-				$basetime = CustomBreakTimeAPI::getBaseBreakTime($item);
-				if($basetime == null) return;
-				$pos = new Vector3($packet->x, $packet->y, $packet->z);
-				$block = $player->getWorld()->getBlock($pos);
-				$time = $basetime->reCaculateBreakTime($block, $item, $player);
-				$this->getAPI()->setBreakStatus($player, true);
-				$this->task[$player->getName()] = new BreakTask($player, $pos, $this->getAPI());
-				$this->getAPI()->getScheduler()->scheduleDelayedTask($this->task[$player->getName()], $time);
-				break;
-			case PlayerActionPacket::ACTION_ABORT_BREAK:
-			case PlayerActionPacket::ACTION_STOP_BREAK:
-				$this->getAPI()->setBreakStatus($player, false);
-				if(isset($this->task[$player->getName()])){
-					$this->task[$player->getName()]->cancel();
-				}
-				break;
-		}
 	}
 
 	/**
